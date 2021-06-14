95512e23153adf491e13351ffeeb6c1e24bf1da4
ianbrown9475 <ian.brown9475@gmail.com>
2021-06-14 12:29:01 -0700

    fix: rename example_script -> example_workflow, fix .gitignore

--- workflows/example_workflow/setup.py
+++ workflows/example_workflow/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='example',
+    name='example_workflow',
     description='Copies an input file to an output file. This script is used '
     'purely as demonstration for containerization',
     version='0.1.0',
